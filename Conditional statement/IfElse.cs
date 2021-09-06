cdefbf369c61c9eec6cafcf3dd3106458cba89e3
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 08:29:50 -0700

    initial

--- Conditional statement/IfElse.cs
+++ Conditional statement/IfElse.cs
@@ -2,24 +2,31 @@ using System;
 
 namespace IfElse 
 {
+    // C# has the following conditional statements:
+
+    // Use if to specify a block of code to be executed, if a specified condition is true
+    // Use else to specify a block of code to be executed, if the same condition is false
+    // Use else if to specify a new condition to test, if the first condition is false
+    // Use switch to specify many alternative blocks of code to be executed
+
     class IfElse
     {
         public static void Main(string[] args)
         {
-            bool complete = true;
+            // SYNTAX
+            // if (condition) 
+            // {
+            // // block of code to be executed if the condition is True
+            // }
 
-            if(complete)
+        int x = 20;
+        int y = 180;
+        if (x > y) 
         {
-            int x = 5;
-
-        }
-        else if(started)
+        Console.WriteLine("x is greater than y");
+        }else
         {
-            x = 3;
-        }
-        else{
-            x = 1;
-            
+            Console.WriteLine("x is smaller than y");
         }
         }
     }
