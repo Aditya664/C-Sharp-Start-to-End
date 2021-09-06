ec32caccda3e2cb15d77055e8eba6c4233f5a913
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 09:44:09 -0700

    initial

---
+++ Basic C# syntax/Syntax.cs
@@ -0,0 +1,27 @@
+using System;
+
+// C# used namespaces to avod name conflicts.
+// C# imports namespaces with using Statement.
+namespace Syntax
+{
+    class Syntax
+    {
+       /*Syntax rule-
+        1.C# is case-sensitive language.
+        2.C# uses semicolon end of the line.
+        3.C# uses curly brackets.
+        4.C# used namespaces to avod name conflicts.
+        5.C# imports namespaces with using Statement.
+        6.C# code goes inside the classes.
+        7.C# supports the access modifier public,private and protected and internal methods.
+*/
+
+        public static void Main(String[] args)
+        {
+            int Int = 10; //Case sensitive
+        //C# uses semicolon end of the line.
+        }//C# uses curly brackets.
+
+   
+    }
+}
