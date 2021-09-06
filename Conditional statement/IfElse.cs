e9e265dae99ac5301fc65528a2e529e3f7f9d980
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 08:22:30 -0700

    initial

---
+++ Conditional statement/IfElse.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace IfElse 
+{
+    class IfElse
+    {
+        public static void Main(string[] args)
+        {
+            bool complete = true;
+
+            if(complete)
+        {
+            int x = 5;
+
+        }
+        else if(started)
+        {
+            x = 3;
+        }
+        else{
+            x = 1;
+            
+        }
+        }
+    }
+}
