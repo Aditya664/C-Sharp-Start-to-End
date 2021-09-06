1d0df4404abd5391c67644222ba70441b192d747
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 09:55:06 -0700

    initial

---
+++ Operators/IncreDecre.cs
@@ -0,0 +1,25 @@
+using System;
+namespace IncreDecre 
+{
+    class IncreDecre 
+    {
+         //Increment and decrement operators
+            //++ - increment operator
+            // -- decrement operator
+            //++ adds 1 ,-- subtract 1
+            //they assign the new value to the variable.
+
+        public static void Main(string[] args)
+        {
+           //increment operator
+            int num = 4;
+            num++;
+            Console.WriteLine(num);
+            //decrement operator
+            int num1 = 5;
+            num1--;
+            Console.WriteLine(num1);
+
+        }
+    }
+}
---
+++ Operators/Modulo.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Modulo
+{
+    class Modulo
+    {
+        // What is Modulus operator?
+        //     -the modulus operator is: %.
+        //     -Gives us the reminder of the division.
+        //     -5/4 gives us a reminder of 1 so 5 % 4 = 1.
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(5 % 4);
+        }
+    }
+}
