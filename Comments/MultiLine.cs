8f067ba046735cdd812d30dec7944f22a9800281
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 09:18:22 -0700

    initial

---
+++ Comments/MultiLine.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MultiLine
+{
+    class MultiLine 
+    {
+        // What is Comments?
+        // -A Comment is line of code that is not executed by the computer.
+        // -Every programming language has them comment.
+        // -used to explain code and make notes in the code.
+        // -3 types of comments:-
+        //     -Single-line-comments 
+        //     -Multi-line-comments 
+        //     -XML-comments 
+
+        public static void Main(System[] args)
+        {
+           /* This is the 
+            multi-line comment */
+        }
+    }
+}
---
+++ Comments/SingleLine.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace SingleLine
+{
+    class SingleLine 
+    {
+        // What is Comments?
+        // -A Comment is line of code that is not executed by the computer.
+        // -Every programming language has them comment.
+        // -used to explain code and make notes in the code.
+        // -3 types of comments:-
+        //     -Single-line-comments 
+        //     -Multi-line-comments 
+        //     -XML-comments 
+
+        public static void Main(System[] args)
+        {
+          //This is single line comments.
+        }
+    }
+}
