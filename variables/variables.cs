d297ff67d35be931a3647cbff4c6a6a2a64b8aa0
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 08:10:53 -0700

    first commit

---
+++ variables/variables.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace VaSSriables
+{
+    //Variables--------
+    //1.Variables store data in computer programming.
+    //2.Each variable has a type and a value.
+    //Every programming language has variables but rules are changed.
+    //in C# the name can only contain letters,digitts and the underscore character(_)
+    //no other special character are allowed in variable names.
+    //The first character must be a letter or underscore.
+    //C# is is case sensitive with variable names.
+    //can't use reserved names for variables. 
+    public class Variables
+    {
+        static void Main(String[] args)
+        {
+            //The main type of variable are---
+            //     1.integers
+            //     2.floats
+            //     3.strings
+            //     4.characters
+            //     5.booleans
+
+            //int
+            int x = 5;
+            int y = 3;
+            int z = -1;
+            int num = 8;
+            //float
+            float y = 3.2f;
+            float x = 3.8f;
+            float num = 0.8f;
+            //string
+            string strin1 = "a";
+            string string2 = "bird";
+            string string3 = " i eat pizza";
+            //char
+            char char1 = 'a';
+            char chsr2 = 'b';
+            char char3 = 'c';
+            //boolean
+            boolean complete = true;
+            boolean incomplete = false;
+
+
+
+        }
+    }
+}
