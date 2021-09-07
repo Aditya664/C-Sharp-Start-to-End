67fc44cf4880c95806a66c4c4100600b7a523b2e
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-07 01:21:49 -0700

    Initial

---
+++ NullCoalescing/NullCoal.cs
@@ -0,0 +1,13 @@
+using System;
+ namespace Exmpletion
+ {
+     class NullCoal
+     {
+         public static void Main(System[] args)
+         {
+            //  Variable = left operand ?? right operand;
+
+            Object obj = null;
+            Object obj1 = obj ?? new Object();         }
+     }
+ }
