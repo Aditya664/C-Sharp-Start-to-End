bec69079b5ce374645184b11dcc225a13cfa4dc0
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-07 04:45:29 -0700

    initial

---
+++ DataTypes/Numbers.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Numbers
+{
+    class Numbers
+    {
+        public static void Main(string[] args)
+     {
+         //number Variables
+
+         int i = 0;
+         i = 5;
+         Console.WriteLine(i + 1);
+         Console.WriteLine(i - 1);
+            Console.WriteLine(i++);
+            Console.WriteLine(i++);
+            Console.WriteLine(--i);
+            Console.WriteLine(i--);
+
+
+        }   
+    }
+}
