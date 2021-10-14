3b38354f0d4c1315575c3fa87faa5f7d422bdda7
Restyled.io <commits@restyled.io>
2021-10-14 20:12:35 +0000

    Restyled by astyle

--- Operators/Nameof.cs
+++ Operators/Nameof.cs
@@ -2,16 +2,16 @@
 
 namespace Nameof
 {
-    class Nameof
-    {
-        // nameof expression allows you to get the name of a variable, type or a function
-        // in a form of a string constant
+class Nameof
+{
+    // nameof expression allows you to get the name of a variable, type or a function
+    // in a form of a string constant
 
-        private static void Main(string[] args)
-        {
-            Console.WriteLine(nameof(Main));
-            Console.WriteLine(nameof(args));
-            Console.WriteLine(nameof(Nameof));
-        }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(nameof(Main));
+        Console.WriteLine(nameof(args));
+        Console.WriteLine(nameof(Nameof));
     }
+}
 }
