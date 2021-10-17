b53f7a8169f4d563b44c991158f8b7a6427a6ea2
Restyled.io <commits@restyled.io>
2021-10-17 10:26:58 +0000

    Restyled by clang-format

--- Comments/XML.cs
+++ Comments/XML.cs
@@ -1,28 +1,23 @@
-﻿namespace XML
-{
-class XML
-{
-    // What are comments?
-    // - A comment is a line of code that is not executed by the computer
-    // - Every major programming language has an ability to write comments
-    // - They are used to explain code and make notes
-    // - There are 3 types of comments:
-    //   + Single-line comments
-    //   + Multi-line comments
-    //   + XML comments
+﻿namespace XML {
+class XML {
+  // What are comments?
+  // - A comment is a line of code that is not executed by the computer
+  // - Every major programming language has an ability to write comments
+  // - They are used to explain code and make notes
+  // - There are 3 types of comments:
+  //   + Single-line comments
+  //   + Multi-line comments
+  //   + XML comments
 
-    /// <summary>
-    ///  This is an example of an XML comment.
-    ///  Hover your mouse over this method to see
-    ///  a tooltip that displays the summary of this method.
-    /// </summary>
-    /// <param name="args">
-    ///  Hover over the argument of this method
-    ///  to see an another comment.
-    /// </param>
-    private static void Main(string[] args)
-    {
-
-    }
+  /// <summary>
+  ///  This is an example of an XML comment.
+  ///  Hover your mouse over this method to see
+  ///  a tooltip that displays the summary of this method.
+  /// </summary>
+  /// <param name="args">
+  ///  Hover over the argument of this method
+  ///  to see an another comment.
+  /// </param>
+  private static void Main(string[] args) {}
 }
 }
