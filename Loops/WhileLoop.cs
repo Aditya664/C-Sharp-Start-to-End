5e6f4816ab935196e9aad88f8aa56899842cb61b
Restyled.io <commits@restyled.io>
2021-10-18 12:11:52 +0000

    Restyled by clang-format

--- Loops/WhileLoop.cs
+++ Loops/WhileLoop.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace WhileLoop
-{
+namespace WhileLoop {
 // LOOPS:-
 // -A loop Execute a piece of code repeatedly until a condition is fulfilled.
 // -Many of the programs that you write will involve loops.
@@ -13,23 +12,21 @@ namespace WhileLoop
 // -A for loop iterates over elements umtil the specified length is reached.
 // -A While loop executes until a specified condition is met;
 // -Care should be taken when using While loop to avoid Infinite loop
-// - An infinite loop is a loop that doesn't terminate i.e This could crash your program or system.
+// - An infinite loop is a loop that doesn't terminate i.e This could crash your
+// program or system.
 
-class WhileLoop
-{
-    public static void Main(String[] args)
-    {
-        // while (condition)
-        // {
-        // // code block to be executed
-        // }
+class WhileLoop {
+  public static void Main(String[] args) {
+    // while (condition)
+    // {
+    // // code block to be executed
+    // }
 
-        int i = 3;
-        while (i > 0)
-        {
-            Console.WriteLine("Loading");
-            i--;
-        }
+    int i = 3;
+    while (i > 0) {
+      Console.WriteLine("Loading");
+      i--;
     }
+  }
 }
 }
