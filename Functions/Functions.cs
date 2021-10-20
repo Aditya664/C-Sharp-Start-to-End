d71c4e07dd8b56f4b7bcff615953d87806f5be7d
Restyled.io <commits@restyled.io>
2021-10-20 08:31:52 +0000

    Restyled by clang-format

--- Functions/Functions.cs
+++ Functions/Functions.cs
@@ -1,24 +1,16 @@
 using System;
 
-namespace Functions
-{
-class Functions
-{
-    // What is a Function?
-    // -Function,or methods, are blocks of code that can be reused throughout our program.
-    // -they make it easy to reuse code without typing it over and over.
-    // -We can make a change in the Function body and not have to change it everywhere the function is called.
-    // -Save the time and make our code more readable and manageable.
+namespace Functions {
+class Functions {
+  // What is a Function?
+  // -Function,or methods, are blocks of code that can be reused throughout our
+  // program. -they make it easy to reuse code without typing it over and over.
+  // -We can make a change in the Function body and not have to change it
+  // everywhere the function is called. -Save the time and make our code more
+  // readable and manageable.
 
-    public static void Main(String[] args)
-    {
-        PrintHi("Hello World!");
-    }
-    //This is the function
-    static void PrintHi(string message)
-    {
-        Console.WriteLine(message);
-    }
-
+  public static void Main(String[] args) { PrintHi("Hello World!"); }
+  // This is the function
+  static void PrintHi(string message) { Console.WriteLine(message); }
 }
 }
--- variables/variables.cs
+++ variables/variables.cs
@@ -1,63 +1,54 @@
 using System;
 
-namespace VaSSriables
-{
-//Variables--------
-//1.Variables store data in computer programming.
-//2.Each variable has a type and a value.
-//Every programming language has variables but rules are changed.
-//in C# the name can only contain letters,digitts and the underscore character(_)
-//no other special character are allowed in variable names.
-//The first character must be a letter or underscore.
-//C# is is case sensitive with variable names.
-//can't use reserved names for variables.
-public class Variables
-{
-    static void Main(String[] args)
-    {
-        //The main type of variable are---
-        //     1.integers
-        //     2.floats
-        //     3.strings
-        //     4.characters
-        //     5.booleans
+namespace VaSSriables {
+// Variables--------
+// 1.Variables store data in computer programming.
+// 2.Each variable has a type and a value.
+// Every programming language has variables but rules are changed.
+// in C# the name can only contain letters,digitts and the underscore
+// character(_) no other special character are allowed in variable names. The
+// first character must be a letter or underscore. C# is is case sensitive with
+// variable names. can't use reserved names for variables.
+public class Variables {
+  static void Main(String[] args) {
+    // The main type of variable are---
+    //     1.integers
+    //     2.floats
+    //     3.strings
+    //     4.characters
+    //     5.booleans
 
-        //int
-        int x = 5;
-        int y = 3;
-        int z = -1;
-        int num = 8;
-        //float
-        float y = 3.2f;
-        float x = 3.8f;
-        float num = 0.8f;
-        //string
-        string strin1 = "a";
-        string string2 = "bird";
-        string string3 = " i eat pizza";
-        //char
-        char char1 = 'a';
-        char chsr2 = 'b';
-        char char3 = 'c';
-        //boolean
-        boolean complete = true;
-        boolean incomplete = false;
+    // int
+    int x = 5;
+    int y = 3;
+    int z = -1;
+    int num = 8;
+    // float
+    float y = 3.2f;
+    float x = 3.8f;
+    float num = 0.8f;
+    // string
+    string strin1 = "a";
+    string string2 = "bird";
+    string string3 = " i eat pizza";
+    // char
+    char char1 = 'a';
+    char chsr2 = 'b';
+    char char3 = 'c';
+    // boolean
+    boolean complete = true;
+    boolean incomplete = false;
 
-        //double
-        double number1 = 20.30d;
-        double number2 = 30.49d;
-        double number3 = 763.435d;
+    // double
+    double number1 = 20.30d;
+    double number2 = 30.49d;
+    double number3 = 763.435d;
 
-        //decimal
-        // This is mostly used for money because of the the higher precision
-        decimal figure1 = 345.24453343m;
-        decimal figure2 = 285.24453343m;
-        decimal figure3 = 965.24453343m;
-
-
-
-
-
-    }
+    // decimal
+    // This is mostly used for money because of the the higher precision
+    decimal figure1 = 345.24453343m;
+    decimal figure2 = 285.24453343m;
+    decimal figure3 = 965.24453343m;
+  }
 }
 }
